2e89678cce4dd78d39fb89677ab81f812a45d7a2
RYAN\azu <blazerod123480@gmail.com>
2024-04-25 20:31:01 +0800

    new_rating not working

--- app.py
+++ app.py
@@ -3,9 +3,14 @@ from dotenv import load_dotenv
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.common.exceptions import NoSuchElementException
+from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from flask import Flask, jsonify, request
+from bs4 import BeautifulSoup
+import json
 
+app = Flask(__name__)
 
 # Load variables from .env file
 load_dotenv()
@@ -21,6 +26,7 @@ try:
     options = webdriver.ChromeOptions()
     options.add_experimental_option("detach", True)
     options.add_argument("--enable-logging=stderr")
+    options.add_argument('--log-level=3')
     driver = webdriver.Chrome(options=options)  # Example for Chrome, you can use other browsers as well
 
     # Open the webpage
@@ -66,6 +72,44 @@ try:
     )
 
     analyze_rating_link.click()
+    print("hi")
+    print("hi2")
+    try:
+        print("hi3")
+        new_rating = WebDriverWait(driver, 10).until(
+            EC.visibility_of_element_located((By.CSS_SELECTOR, '.topRecordTable'))
+        )
+        print("hi4")
+        new_rating = new_rating.get_attribute('outerHTML')
+
+        soup = BeautifulSoup(new_rating, 'html.parser')
+        records=[]
+        
+        
+        for row in soup.find_all('tr', class_='scoreRecordRow')[1:]:
+            record = {}
+            cells = row.find_all('td')
+            record['#'] = cells[0].text.strip()
+            record['Song'] = cells[1].text.strip()
+            record['Chart'] = cells[2].text.strip()
+            record['Level'] = cells[3].text.strip()
+            record['Achv'] = cells[4].text.strip()
+            record['Rank'] = cells[5].text.strip()
+            record['Rating'] = cells[6].text.strip()
+            records.append(record)
+
+        json_data = json.dumps(records, indent=4)
+        print(json_data)
+    except TimeoutException as e:
+        print(e)
 
 except Exception as e:
     print(e)
+
+
+@app.route('/api/data', methods=['GET'])
+def get_data():
+    return jsonify(json_data)
+
+if __name__ == '__main__':
+    app.run(debug=True)
