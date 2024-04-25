82de9640ea6e1bfcc856129e07f4b5d48686b16d
Ryan Choo <blazerod123480@gmail.com>
2024-04-25 23:42:55 +0800

    Start Database and Discord

--- app.py
+++ app.py
@@ -6,11 +6,11 @@ from selenium.common.exceptions import NoSuchElementException
 from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from flask import Flask, jsonify, request
 from bs4 import BeautifulSoup
+from pymongo import MongoClient
 import json
-
-app = Flask(__name__)
+import logging
+import datetime
 
 # Load variables from .env file
 load_dotenv()
@@ -72,44 +72,48 @@ try:
     )
 
     analyze_rating_link.click()
-    print("hi")
-    print("hi2")
-    try:
-        print("hi3")
-        new_rating = WebDriverWait(driver, 10).until(
-            EC.visibility_of_element_located((By.CSS_SELECTOR, '.topRecordTable'))
-        )
-        print("hi4")
-        new_rating = new_rating.get_attribute('outerHTML')
 
-        soup = BeautifulSoup(new_rating, 'html.parser')
-        records=[]
-        
-        
-        for row in soup.find_all('tr', class_='scoreRecordRow')[1:]:
-            record = {}
-            cells = row.find_all('td')
-            record['#'] = cells[0].text.strip()
-            record['Song'] = cells[1].text.strip()
-            record['Chart'] = cells[2].text.strip()
-            record['Level'] = cells[3].text.strip()
-            record['Achv'] = cells[4].text.strip()
-            record['Rank'] = cells[5].text.strip()
-            record['Rating'] = cells[6].text.strip()
-            records.append(record)
+    driver.switch_to.window(driver.window_handles[-1])
+    new_rating_elements = WebDriverWait(driver, 10).until(EC.presence_of_all_elements_located((By.CSS_SELECTOR, '.topRecordTable.songRecordTable')))
 
-        json_data = json.dumps(records, indent=4)
-        print(json_data)
-    except TimeoutException as e:
-        print(e)
+    
+    if len(new_rating_elements) >= 2:
+        # Get the second element
+        second_element = new_rating_elements[1]
+    new_rating = second_element.get_attribute('outerHTML')
+
+    soup = BeautifulSoup(new_rating, 'html.parser')
+    records=[]
+    
+    
+    for row in soup.find_all('tr', class_='scoreRecordRow')[1:]:
+        record = {}
+        cells = row.find_all('td')
+        record['#'] = cells[0].text.strip()
+        record['Song'] = cells[1].text.strip()
+        record['Chart'] = cells[2].text.strip()
+        record['Level'] = cells[3].text.strip()
+        record['Achv'] = cells[4].text.strip()
+        record['Rank'] = cells[5].text.strip()
+        record['Rating'] = cells[6].text.strip()
+        records.append(record)
+
+    current_datetime = datetime.datetime.now().strftime("%d/%m/%Y %H:%M:%S")
+    records.append({'Date': current_datetime})
+    json_data = json.dumps(records, indent=4)
+    print(json_data)
+
+    CONNECTION_STRING = "mongodb://localhost:27017/"
+    client = MongoClient(CONNECTION_STRING)
+    db = client["mydatabase"]
+    collection = db["maimai"]
+    collection.insert_one({'records': records, 'Date': current_datetime})
+
+    latest_object = collection.find_one(sort=[('_id', -1)])
+
+    print(latest_object)
 
 except Exception as e:
-    print(e)
-
-
-@app.route('/api/data', methods=['GET'])
-def get_data():
-    return jsonify(json_data)
-
-if __name__ == '__main__':
-    app.run(debug=True)
+    logging.error("Error: Table not found within 10 seconds.")
+    logging.error(f"Exception message: {e}")
+    logging.error(f"Exception type: {type(e)}")
